734bd4b6cd171498563cf6e57405160ee0d79ce3
Tiffany Prosser <tiffany.prosser1@gmail.com>
2022-11-14 16:11:37 +0000

    Add files via upload

---
+++ Models/City.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace AirQuality.Models
+{
+    public class City
+    {
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
+
+        [JsonPropertyName("city")]
+        public string city { get; set; }
+
+        [JsonPropertyName("count")]
+        public int Count { get; set; }
+
+        [JsonPropertyName("locations")]
+        public int Locations { get; set; }
+
+        [JsonPropertyName("firstUpdated")]
+        public DateTime FirstUpdated { get; set; }
+
+        [JsonPropertyName("lastUpdated")]
+        public DateTime LastUpdated { get; set; }
+
+        [JsonPropertyName("parameters")]
+        public List<string> Parameters { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AirQuality.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
